e56f50a55b1bd71be92b2054e6df6af8a4e4a38a
pipopa <pipopa.710@gmail.com>
2017-12-29 16:15:47 +0900

    弾の描画処理を追加

--- src/render.rs
+++ src/render.rs
@@ -8,6 +8,12 @@ pub struct Render<'a> {
     target: Option<glium::Frame>,
 }
 
+#[derive(Copy, Clone)]
+struct Vertex {
+    position: [f32; 2],
+}
+implement_vertex!(Vertex, position);
+
 impl<'a> Render<'a> {
     pub fn new(display: &'a glium::Display) -> Self {
         let target = display.draw();
@@ -30,15 +36,8 @@ impl<'a> Render<'a> {
         }
     }
 
-    pub fn draw_rectangle(&mut self, pos: Position, radius: usize) {
-        #[derive(Copy, Clone)]
-        struct Vertex {
-            position: [f32; 2],
-        }
-        implement_vertex!(Vertex, position);
-
+    pub fn draw_rectangle(&mut self, pos: Position, radius: f32) {
         let (width, height) = self.display.get_framebuffer_dimensions();
-
         let pos = Position {
             x: pos.x / (width as f32 / 2.0) - 1.0,
             y: pos.y / (height as f32 / 2.0) - 1.0,
@@ -54,7 +53,6 @@ impl<'a> Render<'a> {
             ib
         };
 
-        let radius = radius as f32;
         let half_width = width as f32 / 2.0;
         let half_height = height as f32 / 2.0;
 
@@ -92,6 +90,57 @@ impl<'a> Render<'a> {
             glium::Program::from_source(self.display, vertex_shader_src, fragment_shader_src, None)
                 .unwrap();
 
+        self.target
+            .as_mut()
+            .unwrap()
+            .draw(
+                &vertex_buffer,
+                &indices,
+                &program,
+                &glium::uniforms::EmptyUniforms,
+                &Default::default(),
+            )
+            .unwrap();
+    }
+
+    pub fn draw_circle(&mut self, pos: Position, radius: f32, a: f32, b: f32) {
+        let (width, height) = self.display.get_framebuffer_dimensions();
+        let mut shape = Vec::new();
+        let n: i32 = 200;
+        for i in 0..n {
+            let x = pos.x + a * radius * (2.0 * 3.1415 * (i as f32 / n as f32)).cos();
+            let y = pos.y + b * radius * (2.0 * 3.1415 * (i as f32 / n as f32)).sin();
+            let vertex = Vertex {
+                position: [
+                    x / (width as f32 / 2.0) - 1.0,
+                    y / (height as f32 / 2.0) - 1.0,
+                ],
+            };
+            shape.push(vertex);
+        }
+
+        let vertex_buffer = glium::VertexBuffer::new(self.display, &shape).unwrap();
+        let indices = glium::index::NoIndices(glium::index::PrimitiveType::TriangleFan);
+
+        let vertex_shader_src = r#"
+            #version 140
+            in vec2 position;
+            void main() {
+                gl_Position = vec4(position, 0.0, 1.0);
+            }
+        "#;
+
+        let fragment_shader_src = r#"
+            #version 140
+            out vec4 color;
+            void main() {
+                color = vec4(1.0, 0.0, 0.0, 1.0);
+            }
+        "#;
+
+        let program =
+            glium::Program::from_source(self.display, vertex_shader_src, fragment_shader_src, None)
+                .unwrap();
         self.target
             .as_mut()
             .unwrap()
